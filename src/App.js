13855a8bccbd892aabe7639ab26d9cd282902e14
smimoto <smith9128@hotmail.com>
2021-12-28 00:24:58 +0100

    finished

--- src/App.js
+++ src/App.js
@@ -1,25 +1,106 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import { Switch, Typography } from '@material-ui/core';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import {ThemeProvider, createTheme} from '@material-ui/core/styles'
+import { Navbar, Products, Cart, Checkout } from './components';
+import { commerce } from './lib/commerce';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+  const [products, setProducts] = useState([]);
+  const [cart, setCart] = useState({});
+  const [order, setOrder] = useState({});
+  const [errorMessage, setErrorMessage] = useState('');
+
+  const fetchProducts = async () => {
+    const { data } = await commerce.products.list();
+
+    setProducts(data);
+  };
+
+  const fetchCart = async () => {
+    setCart(await commerce.cart.retrieve());
+  };
+
+  const handleAddToCart = async (productId, quantity) => {
+    const item = await commerce.cart.add(productId, quantity);
+
+    setCart(item.cart);
+  };
+
+  const handleUpdateCartQty = async (lineItemId, quantity) => {
+    const response = await commerce.cart.update(lineItemId, { quantity });
+
+    setCart(response.cart);
+  };
+
+  const handleRemoveFromCart = async (lineItemId) => {
+    const response = await commerce.cart.remove(lineItemId);
+
+    setCart(response.cart);
+  };
+
+  const handleEmptyCart = async () => {
+    const response = await commerce.cart.empty();
+
+    setCart(response.cart);
+  };
+
+  const refreshCart = async () => {
+    const newCart = await commerce.cart.refresh();
+
+    setCart(newCart);
+  };
+
+  const handleCaptureCheckout = async (checkoutTokenId, newOrder) => {
+    try {
+      const incomingOrder = await commerce.checkout.capture(checkoutTokenId, newOrder);
+
+      setOrder(incomingOrder);
+
+      refreshCart();
+    } catch (error) {
+      setErrorMessage(error.data.error.message);
+    }
+  };
+
+  useEffect(() => {
+    fetchProducts();
+    fetchCart();
+  }, []);
+
+const [darkMode, setDarkMode] = useState(true)
+
+const theme = createTheme({
+  palette:{
+    type: darkMode ? "dark" : "light"
+  }
+})
+
+    return (
+      
+      <ThemeProvider theme={theme}>
+        <Router>
+        <div>
+        <Switch checked={darkMode} onChange={()=> setDarkMode(!darkMode)} />
+
+            <Navbar totalItems={cart.total_items} theme={theme}/>
+                               <Switch checked={darkMode} onChange={()=> setDarkMode(!darkMode)} />
+
+            <Routes>
+                <Route path='/' element={<Products products = {products} onAddToCart = {handleAddToCart}/>} />
+                <Route path='/cart' element={<Cart cart={cart} handleUpdateCartQty={handleUpdateCartQty} handleRemoveFromCart={handleRemoveFromCart} handleEmptyCart={handleEmptyCart} />} />
+                <Route path='/checkout' element={<Checkout 
+                    cart={cart} 
+                    order={order}
+                    onCaptureCheckout={handleCaptureCheckout}
+                    error={errorMessage}
+                />} />
+            </Routes>
+
+        </div>
+        </Router>
+        </ThemeProvider>
+    )
 }
 
-export default App;
+export default App
